6085b7d486da57d43be3f6ef2fd68242ea521749
savenkovv <vov_gam@mail.ru>
2021-10-27 22:33:42 +0300

    start

---
+++ test1_page.php
@@ -0,0 +1,57 @@
+<?php
+  require 'functions.php';
+  require 'confDB.php';
+?>  
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>TEST1</title>
+  <link rel="stylesheet" href="css/font-awesome.min.css">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/bootstrap-grid.min.css">
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="icon" href="img/icon.ico" type="image/x-icon">
+</head>
+<body>
+
+  <header class="main-header">
+    <div class="container d-flex justify-content-around align-items-center">
+      TEST1
+    </div>
+  </header>
+
+  <section>
+
+    <div class="container">
+      <div class="shadow-lg bg-white main-area p-4 col-12">
+        <form action="" class="d-flex flex-column col-12">
+          <div class="d-flex flex-row form-group align-items-center col-10 justify-content-start m-0">
+            <div class="word1 col-3"><b>Probably</b></div>
+            <input type="text" class="word2 m-2 col-3">
+          </div><hr class="hr1">
+          <div class="d-flex flex-row form-group align-items-center col-10 justify-content-start m-0">
+            <div class="word1 col-3"><b>Probably</b></div>
+            <input type="text" class="word2 m-2 col-3">
+          </div><hr class="hr1">
+          <div class="d-flex flex-row form-group align-items-center col-10 justify-content-start m-0">
+            <div class="word1 col-3"><b>Probably</b></div>
+            <input type="text" class="word2 m-2 col-3">
+          </div><hr class="hr1">
+          <div class="d-flex flex-row form-group align-items-center col-10 justify-content-start m-0">
+            <div class="word1 col-3"><b>Probably</b></div>
+            <input type="text" class="word2 m-2 col-3">
+          </div><hr class="hr1">
+          <button id="create" name="create" type="submit" class="btn btn-primary col-3">Проверить</button>
+        </form>
+      </div>
+    </div>
+      
+
+
+  </section>
+</body>
+</html>
+
