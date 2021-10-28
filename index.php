a96b96b466ac4dfd108ca2085a70ee0a2bcd238b
savenkovv <vov_gam@mail.ru>
2021-10-28 13:53:59 +0300

    123

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Список слов</title>
+  <title>СЛОВАРЬ</title>
   <link rel="stylesheet" href="css/font-awesome.min.css">
   <link rel="stylesheet" href="css/bootstrap.min.css">
   <link rel="stylesheet" href="css/bootstrap-grid.min.css">
@@ -18,8 +18,12 @@
 <body>
 
   <header class="main-header">
-    <div class="container d-flex justify-content-around align-items-center">
-      TEST1
+    <div class="container d-flex justify-content-between align-items-center">
+      <div class="page-name"><b>СЛОВАРЬ</b></div>
+      <div class="links">
+        <a href="test_eng.php" class="border rounded p-2">Тест ENG</a>
+        <a href="test_rus.php" class="border rounded p-2">Тест RUS</a>
+      </div>
     </div>
   </header>
 
@@ -27,13 +31,10 @@
 
     <div class="container">
       <div class="shadow-lg bg-white main-area p-4 col-12">
-        <button type="button" class="btn btn-success m-4" data-toggle="modal" data-target="#exampleModal">
-            Добавить слово
-        </button>
-      </div>
-    </div>
-      
-    <div class="m-2">
+          <button type="button" class="btn btn-success m-4" data-toggle="modal" data-target="#exampleModal">
+            Добавить новое слово
+          </button>
+        <div class="m-2">
             <?php
                 if (isset($_SESSION['success'])){
                     display_flash_message("success");
@@ -48,33 +49,21 @@
           <div class="modal-dialog">
             <div class="modal-content">
               <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Добавить пользователя</h5>
+                <h5 class="modal-title" id="exampleModalLabel">Добавить новое слово</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                   <span aria-hidden="true">&times;</span>
                 </button>
               </div>
-              <form class="form-group" action="register.php" method="post">
+              <form class="form-group" action="new_word.php" method="post">
                 <div class="modal-body d-flex flex-column">
-                    <div class="form-group">
-                      <label for="exampleInputEmail1">Имя</label>
-                      <input name="user_name" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" required>
-                    </div>
-                    <div class="form-group">
-                      <label for="exampleInputEmail1">Логин</label>
-                      <input name="email" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" required>
-                    </div>
-                    <div class="form-group">
-                      <label for="exampleInputPassword1">Пароль</label>
-                      <input name="password" type="password" class="form-control dissable_field_pass" id="exampleInputPassword1" required>
-                    </div>
-                    <div class="form-check form-check-inline w-100">
-                      <input name="admin" class="form-check-input ml-3" type="checkbox" id="inlineCheckbox1" value="admin">
-                      <label class="form-check-label ml-2" for="inlineCheckbox1">Администратор</label>
-                    </div>
-                    <div class="form-check form-check-inline w-100">
-                      <input name="new_pass" class="form-check-input ml-3" type="checkbox" id="inlineCheckbox2" value="1">
-                      <label class="form-check-label ml-2" for="inlineCheckbox2">Сменить пароль при входе</label>
-                    </div>
+                  <div class="form-group">
+                    <label for="exampleInputEmail1">Имя</label>
+                    <input name="user_name" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" required>
+                  </div>
+                  <div class="form-group">
+                    <label for="exampleInputEmail1">Логин</label>
+                    <input name="email" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" required>
+                  </div>
                 </div>
                 <div class="modal-footer">
                   <button type="button" class="btn btn-danger" data-dismiss="modal">Отмена</button>
@@ -87,6 +76,10 @@
 
 
   </section>
+  <script src="js/jquery-3.5.1.min.js"></script>
+  <script src="js/bootstrap.bundle.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="https://kit.fontawesome.com/7fb1e43a84.js" crossorigin="anonymous"></script>
 </body>
 </html>
 
